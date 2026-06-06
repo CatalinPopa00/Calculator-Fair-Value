815982914ee1727775dd3e1d0a0c807bdf8c9686
CatalinPopa00 <catalinpopa2000@gmail.com>
2026-06-06 13:51:03 +0300

    GTB PEG

---
+++ test_adbe3.py
@@ -0,0 +1,8 @@
+﻿import api.index as idx
+from fastapi import Response
+try:
+    data = idx.get_valuation('ADBE', Response(), skip_peers=True, force_refresh=True)
+    print('3y cagr:', data['company_profile'].get('eps_growth_3y_cagr'))
+except Exception as e:
+    import traceback
+    print(traceback.format_exc())
---
+++ test_adbe4.py
@@ -0,0 +1,8 @@
+﻿import api.index as idx
+from fastapi import Response
+try:
+    data = idx.get_valuation('ADBE', Response(), skip_peers=True, force_refresh=True)
+    print('5y cagr custom:', data['company_profile'].get('cagr_5y_custom'))
+except Exception as e:
+    import traceback
+    print(traceback.format_exc())
