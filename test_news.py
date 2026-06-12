2f1a429f48d2124c646d215f8e95df88f4b2d60d
CatalinPopa00 <catalinpopa2000@gmail.com>
2026-06-12 07:21:51 +0300

    Optimizare 3

--- test_news.py
+++
@@ -1,7 +0,0 @@
-import yfinance as yf
-import json
-
-stock = yf.Ticker("AAPL")
-news = stock.news
-if news:
-    print(json.dumps(news[0], indent=2))
--- test_yf_news.py
+++
@@ -1,9 +0,0 @@
-import yfinance as yf
-import json
-
-ticker = yf.Ticker("AAPL")
-news = ticker.news
-if news:
-    print(json.dumps(news[0], indent=2, default=str))
-else:
-    print("No news found")
